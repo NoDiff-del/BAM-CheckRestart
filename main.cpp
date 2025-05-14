1fea9796e64f1aa0e8871d936264ac9b4cb88821
Orbdiff <160801916+Orbdiff@users.noreply.github.com>
2025-05-13 22:08:24 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "BamSysInspector.h"
+#include "SeDebugPrivilege.hh"
 
 int main() {
+    if (!EnableDebugPrivilege()) {
+        std::wcerr << L"[!!] Could not enable SE_DEBUG_NAME privilege. Exiting." << std::endl;
+        Sleep(2000);
+        return 1;
+    }
 
     std::wcout << L"Checking bam.sys presence...\n";
 
